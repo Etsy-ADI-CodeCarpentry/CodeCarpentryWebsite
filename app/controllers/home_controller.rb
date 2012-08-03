509233b57f4fbd5d58aedc3b5edc3a766d10dd67
Zack Newman <znewman01@gmail.com>
2012-08-02 22:30:17 -0400

    Add Gravatar support
    Figure out how to use Gravatar with hardcoded email addresses.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
+require 'digest/md5'
+
 class HomeController < ApplicationController
   def show
+      @hashes = [Digest::MD5.hexdigest('shardisty@etsy.com'.strip.downcase),Digest::MD5.hexdigest('znewman01@gmail.com'.strip.downcase)]
   end
 end
