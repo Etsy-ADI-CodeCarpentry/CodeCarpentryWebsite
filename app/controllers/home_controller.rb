834cb864ea3bde62b2d3c9a4aa650761b2838f4d
Zack Newman <znewman01@gmail.com>
2012-08-06 21:55:31 -0400

    Move Github querying to Repository model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,7 @@
 require 'digest/md5'
-require 'github_api'
 
 class HomeController < ApplicationController
   def show
-    org = 'Etsy-ADI-CodeCarpentry' # todo: make this a config option
-    github = Github.new # todo: don't initialize every time
-    # todo: move this into a model
-    @repo_list = []
-    repos = github.repos.list org: org
-    repos.each do |repo|
-      tmp = {}
-      tmp[:name] = repo.name
-      contributors = github.repos.contributors org, repo.name
-      tmp[:contributors] = contributors.reject { |contributor|
-        contributor.url.include? '/orgs/'
-      }.map { |contributor|
-        {:login => contributor.login, :avatar => contributor.avatar_url}
-      }
-      @repo_list << tmp
-    end
+      @repos = Repository.each
   end
 end
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,5 +1,27 @@
+require 'github_api'
+
 class Repository
   include Mongoid::Document
   field :name, type: String
   embeds_many :contributors
+
+  def self.fetch_from_github
+    github = Github.new
+    org = 'Etsy-ADI-CodeCarpentry' # todo: make this a config option
+    repos = github.repos.list org: org
+    repos.each do |repo_hash|
+      repo = Repository.new(name: repo_hash.name)
+      contributors_response = github.repos.contributors org, repo.name
+      repo.contributors = contributors_response.reject { |contributor|
+        contributor.url.include? '/orgs/'
+      }.map { |contributor|
+        Contributor.create(
+          :login => contributor.login,
+          :avatar_url => contributor.avatar_url,
+          :repository => repo
+        )
+      }
+      repo.upsert
+    end
+  end
 end
