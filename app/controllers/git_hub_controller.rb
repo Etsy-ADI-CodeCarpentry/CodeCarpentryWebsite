bede2dd79f04a6f2e396e252bca442c0dfd0e0f7
Zack Newman <znewman01@gmail.com>
2012-08-29 20:29:29 -0400

    Remove extraneous code for manual refresh
    Get rid of route/controller

--- app/controllers/git_hub_controller.rb
+++
@@ -1,6 +0,0 @@
-class GitHubController < ApplicationController
-  def refresh
-    Repository.fetch_from_github
-    redirect_to :controller => 'home', :action => 'show'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,6 @@ CodeCarpentryWebsite::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root to: 'home#show'
-  match 'github/refresh' => 'gitHub#refresh'
 
   # See how all your routes lay out with "rake routes"
 
